5fc63c372594f308820c03327d7c8fe7fce5d880
Miri <mirifurman@gmail.com>
2022-11-06 09:05:43 +0200

    06.11.22

---
+++ 4.1_boxes/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+
+      <div className="box1">
+        <div className="box2">
+          <div className="box3">
+            <div className="box4"></div>
+            <div className="box4"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
